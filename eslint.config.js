c1a7eec1225c36e19180f13ab7872730165c643d
Adam Burdette <alburdet619@gmail.com>
2025-10-27 16:18:44 -0400

    Add cspell
    Added as eslint config and fixed all issues.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,22 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
+const expoConfig = require("eslint-config-expo/flat.js");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const cspellESLintPluginRecommended = require("@cspell/eslint-plugin/recommended");
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
+  cspellESLintPluginRecommended,
   {
     ignores: ["dist/*"],
+    rules: {
+      "@cspell/spellchecker": [
+        "warn",
+        {
+          configFile: "./cspell.config.yaml",
+        },
+      ],
+    },
   },
 ]);
