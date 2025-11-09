2fdce255311cbe059547e232a70057e43314f2e1
Adam Burdette <alburdet619@gmail.com>
2025-11-09 12:24:12 -0500

    New models generation
    This is to account for our new error type.

--- src/api/models/index.msw.ts
+++ src/api/models/index.msw.ts
@@ -1,7 +1,7 @@
-export { getFactionsMock } from './factions/factions.msw';
-export { getGlobalMock } from './global/global.msw';
 export { getDataMock } from './data/data.msw';
-export { getAgentsMock } from './agents/agents.msw';
 export { getSystemsMock } from './systems/systems.msw';
+export { getGlobalMock } from './global/global.msw';
+export { getFactionsMock } from './factions/factions.msw';
+export { getAgentsMock } from './agents/agents.msw';
 export { getContractsMock } from './contracts/contracts.msw';
 export { getFleetMock } from './fleet/fleet.msw';
