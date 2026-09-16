d35e4e03c974c4b233d8796ecb24b63c80d5986e
Adam Burdette <alburdet619@gmail.com>
2026-09-16 16:53:52 -0400

    Fix app nav theming

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+} from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { PaperProvider } from 'react-native-paper';
+import { adaptNavigationTheme, PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { RootStack } from './src/navigation/RootNavigator';
@@ -12,13 +16,23 @@ import './src/api/interceptors';
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 5 * 60 * 1000 } },
 });
+const { DarkTheme: adaptedNavigationTheme } = adaptNavigationTheme({
+  materialDark: voidTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+const navigationTheme = {
+  ...adaptedNavigationTheme,
+  fonts: NavigationDarkTheme.fonts,
+};
 
 const App = () => {
   return (
-    <GestureHandlerRootView>
+    <GestureHandlerRootView
+      style={[styles.root, { backgroundColor: voidTheme.colors.background }]}
+    >
       <PaperProvider theme={voidTheme}>
         <SafeAreaProvider>
-          <NavigationContainer>
+          <NavigationContainer theme={navigationTheme}>
             <QueryClientProvider client={queryClient}>
               <RootStack />
             </QueryClientProvider>
@@ -28,5 +42,9 @@ const App = () => {
     </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  root: { flex: 1 },
+});
 
 export default App;
